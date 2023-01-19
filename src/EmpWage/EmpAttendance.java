3ca76f928bf7a095ee659c6cc32bdb3771d7b92a
Mayuri2727 <mayuriasabe107@gmail.com>
2023-01-19 12:27:09 +0530

    Employee Attendance

--- src/EmpWage/EmpAttendance.java
+++ src/EmpWage/EmpAttendance.java
@@ -3,6 +3,15 @@ package EmpWage;
 public class EmpAttendance {
     public static void main(String[] args) {
         System.out.println("Welcome to Employee Wage Computation Program");
-
+        int Emp_Full_Time = 1;
+        double Attendance = Math.floor(Math.random() *10)%2;
+        System.out.println("Attendance of emp: " +Attendance);
+        if (Attendance == Emp_Full_Time) {
+            System.out.println("Employee is Present");
+         } else {
+            System.out.println("Employee is Absent");
+         }
+        }
     }
-}
+
+
